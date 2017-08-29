5a54c26969156d3dca8fca95b185366ff91d696d
SychevOleh <sychovoleh@gmail.com>
2017-08-29 23:36:26 +0300

    added jquery, button for moving, functions: sclroll to validation & hide button when already scolled

--- examples/greedy.js
+++
@@ -1,4 +0,0 @@
-var str = '"petya","masha","elena","manya"'; console.log(str.match(/".*"/g));    // жадное, заматчит всю строку
-console.log(str.match(/".*?"/g));   // недажное, выдаст массив имен
-console.log(str.match(/"[^"]*"/g)); // не может быть жадным или
-                                    // нежадным по своей сути
